2a64147407805c8c5bd2b14ecf0c7a6630b24944
AnFlGue <mokshaflores@icloud.com>
2024-08-24 11:59:30 +0200

    02 refactoring Flask_Form

--- 00-Basic-Flask-Form.py
+++ 00-Basic-Flask-Form.py
@@ -1,35 +1,36 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
-# Configure a secret SECRET_KEY
-# We will later learn much better ways to do this!!
+# Configure a secret key for the Flask application.
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-# Now create a WTForm Class
-# Lots of fields available:
-# http://wtforms.readthedocs.io/en/stable/fields.html
-class InfoForm(FlaskForm):
-    '''
-    This general class gets a lot of form about puppies.
-    Mainly a way to go through many of the WTForms Fields.
-    '''
+
+# Define a WTForm class for collecting the information.
+
+class my_InfoForm(FlaskForm):
+    """
+    This class represents a form to collect information about with
+     use of WTForms fields.
+    """
     breed = StringField('What breed are you?')
     submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Set the breed to a boolean False.
-    # So we can use it in an if statement in the html.
+    '''Initialize the breed variable to False to be used
+    in the HTML template to conditionally display content.
+'''
     breed = False
-    # Create instance of the form.
-    form = InfoForm()
-    # If the form is valid on submission (we'll talk about validation next)
+    # Create an instance of the InfoForm.
+    form = my_InfoForm()
+    # Check if the form is valid upon submission.
     if form.validate_on_submit():
-        # Grab the data from the breed on the form.
+        # Retrieve the data from the form.
         breed = form.breed.data
-        # Reset the form's breed data to be False
+        # Reset the breed data in the form to an empty string.
         form.breed.data = ''
     return render_template('00-home.html', form=form, breed=breed)
 
